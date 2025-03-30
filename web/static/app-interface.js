5342799b9d384f7716690cbb6f4e7bd6ddc77d0d
Yanone <post@yanone.de>
2025-03-30 18:45:31 +0200

    Heavier names

--- web/static/app-interface.js
+++ web/static/app-interface.js
@@ -340,7 +340,7 @@ class FontSource {
     }
 
     html() {
-        html = `<li sourcefontid="${this.options.data["fileName"]}"><div class="${this.options.data.isItalic ? "italic" : ""}">${this.options.data["fileName"]}<br />`;
+        html = `<li sourcefontid="${this.options.data["fileName"]}"><div class="${this.options.data.isItalic ? "italic" : ""}"><span class="name">${this.options.data["fileName"]}</span><br />`;
         html += `${this.options.data["type"]} ${this.options.data["weightClass"]} (${this.options.data["size"]}kB)`;
         html += "</div></li>";
         return html;
@@ -358,7 +358,7 @@ class FontTarget {
         return html;
     }
     innerHTML() {
-        html = `<div class="${this.options.isItalic ? "italic" : ""}">${this.options.sourceFont}${this.options.needsCompilation ? " <span class='material-symbols-outlined' title='This font needs to be re-generated'>change_circle</span>" : ""}<br />`;
+        html = `<div class="${this.options.isItalic ? "italic" : ""}"><span class="name">${this.options.sourceFont}</span>${this.options.needsCompilation ? " <span class='material-symbols-outlined' title='This font needs to be re-generated'>change_circle</span>" : ""}<br />`;
         html += `<span class="visiblewhenidle">`
         if (this.options.size_uncompressed) {
             html += `uncmpr:${this.options.size_uncompressed}kB (${Math.round(100 * this.options.size_uncompressed / this.options.source_size)}%)`
